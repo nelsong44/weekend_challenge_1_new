b66c76bca97fa0a5d90adc0d7589f6e34bc5fa92
nelsong44 <nelsong44@gmail.com>
2017-06-25 13:58:23 -0500

    very basic element styling layout complete

--- client.js
+++ client.js
@@ -1,20 +1,21 @@
 console.log(peopleArray);
  $(document).ready(function() {
    console.log('JQ linked');
-
    //on page load, display first person and their shoutout from peopleArray
-   $('.antares').append('<p>' +  peopleArray[0].name + '</p>' +
-   '</p>' + peopleArray[0].shoutout + '</p>' +
-   '<p>1/21</p>');
-
+   $('.name').append(peopleArray[0].name);
+   $('.index').append('1/21');
+   $('.shoutout').append(peopleArray[0].shoutout);
    //on click of 'Next' call nextPerson()
    $('#next').on('click', nextPerson);
    //on click of 'Previous' call previousPerson()
    $('#previous').on('click', previousPerson);
- });
-
+ }); // end onReady
+ 
+// globals
 var i = 0;
 var order = 1;
+
+// on button click, display next person object on DOM from peopleArray
 function nextPerson() {
   i += 1;
   order += 1;
@@ -22,28 +23,27 @@ function nextPerson() {
     i = 0;
     order = 1;
   }
-  $('.antares').html('<p>' +  peopleArray[i].name + '</p>' +
-  '</p>' + peopleArray[i].shoutout + '</p>' +
-  '<p>' + (order) + '/21</p>');
-  console.log(i);
-  console.log(order);
+  $('.name').text(peopleArray[i].name);
+  $('.index').text(order + '/21');
+  $('.shoutout').text(peopleArray[i].shoutout);
 }// end nextPerson
 
+// on button click, display previous person object on DOM from peopleArray
 function previousPerson() {
   i -= 1;
   order -= 1;
   if(i < 0) {
     i = peopleArray.length-1;
     order = peopleArray.length;
-    $('.antares').html('<p>' +  peopleArray[i].name + '</p>' +
-    '</p>' + peopleArray[i].shoutout + '</p>' +
-    '<p>' + (order) + '/21</p>');
+    $('.name').text(peopleArray[i].name);
+    $('.index').text(order + '/21');
+    $('.shoutout').text(peopleArray[i].shoutout);
   }
   else {
-  $('.antares').html('<p>' +  peopleArray[i].name + '</p>' +
-  '</p>' + peopleArray[i].shoutout + '</p>' +
-  '<p>' + (order) + '/21</p>');
-  }
+    $('.name').text(peopleArray[i].name);
+    $('.index').text(order + '/21');
+    $('.shoutout').text(peopleArray[i].shoutout);
+    }
   console.log(i);
   console.log(order);
 
